1f77aa13cdc83defc632d274e5059b400129912c
quantrungduc <quantrungduc.vn@gmail.com>
2026-05-27 17:36:25 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'\govtoolvietnam\',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
